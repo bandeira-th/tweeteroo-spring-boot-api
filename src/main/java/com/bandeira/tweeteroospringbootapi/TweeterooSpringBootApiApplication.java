6cdd42705dca34a0c84d7a0f9b156333eeb8d283
bandeira-th <bandeira.trabalho@gmail.com>
2024-05-08 13:58:28 -0300

    initial commit

---
+++ src/main/java/com/bandeira/tweeteroospringbootapi/TweeterooSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.bandeira.tweeteroospringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeterooSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeterooSpringBootApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bandeira/tweeteroospringbootapi/TweeterooSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandeira.tweeteroospringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweeterooSpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
