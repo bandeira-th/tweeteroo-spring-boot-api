e836688e5782b651f8f601d5e1765673f8eca171
bandeira-th <bandeira.trabalho@gmail.com>
2024-06-18 10:13:05 -0300

    refactor: changed denpendency injections from @Autowired to constructor injection following best practices

--- src/main/java/com/bandeira/tweeteroospringbootapi/controllers/UserController.java
+++ src/main/java/com/bandeira/tweeteroospringbootapi/controllers/UserController.java
@@ -3,24 +3,38 @@ package com.bandeira.tweeteroospringbootapi.controllers;
 import com.bandeira.tweeteroospringbootapi.dtos.CreateUserDto;
 import com.bandeira.tweeteroospringbootapi.services.UserService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("/sign-up")
-public class UserController {
+@RequestMapping("/users")
+class UserController {
 
-    @Autowired
-    private UserService userService;
 
-    @PostMapping
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/sign-up")
     public HttpStatus saveUser(@RequestBody @Valid CreateUserDto req) {
-        
-        userService.save(req);
+        userService.saveUser(req);
         return HttpStatus.CREATED;
     }
+/*
+    @GetMapping(value = "/{username}")
+    public ResponseEntity<GetUserResponseDto> getUser(@PathVariable String username){
+
+        User userRecord = userService.getUserByUsername(username);
+        GetUserResponseDto getUserResponseDto = userMapper.userEntityToDto(userRecord);
+        return ResponseEntity.status(HttpStatus.OK).body(getUserResponseDto);
+
+
+    }
+*/
 }
